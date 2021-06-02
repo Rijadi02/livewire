e9139663473362ef9fa2c08992d945e80b1bfe63
Rijadi's Server <adiveni0@gmail.com>
2021-06-02 16:01:52 +0200

    image upload

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -4,41 +4,64 @@ namespace App\Http\Livewire;
 
 use App\Models\Comment;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Str;
 
 class Comments extends Component
 {
     use WithPagination;
-
+    use WithFileUploads;
     public $newComment;
+    public $image;
+    protected $listeners = ['fileUpload' => 'handleFileUpload'];
 
+    public function handleFileUpload ($imagedata){
+        $this->image = $imagedata;
+    }
 
     public function updated($field){
         $this->validateOnly($field,['newComment'=>'required|max:255']);
     }
 
-
     public function addComment(){
         $this->validate(
             ['newComment'=>'required']
         );
-
-        $createdComment = Comment::create(['body' =>$this->newComment, 'user_id' => 1 ]);
-        // $this->comments->prepend($createdComment);
+        $image = $this->storeImage();
+        $createdComment = Comment::create(
+            ['body' =>$this->newComment,
+            'user_id' => 1,
+            'image' => $image ]);
         $this->newComment = "";
         session()->flash('message','Comment added successfully 😃');
-
-
     }
 
     public function remove($commentId){
         $comment = Comment::find($commentId);
+        Storage::disk('public')->delete($comment->image);
         // $this->comments = $this->comments->except($commentId);
         $comment->delete();
         session()->flash('message','Comment deleted succesfully 😅');
+    }
 
+    public function storeImage(){
+        if(!$this->image){
+            return null;
+        }
 
+        $img = Image::make($this->image)->encode('jpg');
+        $name = Str::random().'.jpg';
+        Storage::disk('public')->put($name, $img);
+        return $name;
+    }
+
+    public function getImagePath()
+    {
+        return Storage::disk('public')->url($this->image);
     }
 
     public function render()
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -3,6 +3,13 @@
 
     <div class="row">
         <div class="container">
+            <section>
+                @if($image)
+                <img src="{{$image}}" width="200" alt="">
+                @endif
+                <input type="file" id="image" wire:change="$emit('fileChoosen')">
+            </section>
+
             <div class="d-flex">
                 <input type="text" name="body" class="form-control  ml-5" wire:model.lazy="newComment">
                 <button type="submit" class="btn btn-primary ml-5 mr-5" >Submit</button>
@@ -20,27 +27,34 @@
 
             <div class="card mt-5">
                <div class="card-header">
-                 Featured
+                @if($comment->image)
+                <img src="http://127.0.0.1:8000/storage/{{$comment->image}}" alt="">
+                @endif
                </div>
                <div class="card-body">
                  <h5 class="card-title">{{$comment->creator->name}}</h5>
                  <p class="card-text">{{$comment->body}} <br></p>
                  <a href="#" class="btn btn-primary">{{$comment->created_at->diffForHumans()}}</a>
                  <a href="#" class="btn btn-danger" wire:click="remove({{$comment->id}})" >Delete</a>
+                </a>
 
-                 </a>
                </div>
              </div>
-
-
-
-
-
             @endforeach
         </div>
     </div>
     {{$comments->links('pagination-links')}}
 
 
-
+    <script>
+        Livewire.on('fileChoosen', () => {
+           let inputField = document.getElementById('image');
+           let file = inputField.files[0];
+           let reader = new FileReader();
+            reader.onloadend = () => {
+                window.Livewire.emit('fileUpload', reader.result);
+            }
+            reader.readAsDataURL(file);
+        })
+    </script>
 </form>
